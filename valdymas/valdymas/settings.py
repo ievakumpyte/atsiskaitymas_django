781f60a515fd9cb041b4dccdd28749c69186b171
Ieva Kumpyte <ieva.kumpytee@gmail.com>
2022-10-25 22:51:15 +0300

    pridetas requirements.txt

--- valdymas/valdymas/settings.py
+++ valdymas/valdymas/settings.py
@@ -130,7 +130,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = 'projektai/projects/'
+LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
